fd3c24370de4e7d7d6f99b662e378c9600cea472
Emanoel Timbo <138140487+Emanoel029@users.noreply.github.com>
2023-11-13 15:53:33 -0300

    Condicionais 2 exemplo

--- Javascript_basico/script.js
+++ Javascript_basico/script.js
@@ -4,10 +4,11 @@ function somar() {
   const div = document.querySelector("div");
   const total = Number(div.innerText) + 1;
   if (total < 10) {
-    div.innerText = total;
+    div.innerText = total; //Imprimindo na tela o total
   } else {
     console.log("Não é possível adcionar mais números!");
   }
 }
-
-botao.addEventListener("click", somar);
+if (botao) {
+  botao.addEventListener("click", somar); //evento do botão
+}
